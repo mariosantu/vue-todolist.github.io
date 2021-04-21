59b31507915455036966105c7c66c0780f9faf89
Mario Santucci <mario.santucci95@gmail.com>
2021-04-21 13:47:27 +0200

    creazione repo + layout

---
+++ js/script.js
@@ -0,0 +1,17 @@
+var app = new Vue( {
+
+	el: '#root',
+
+	data: {
+		userTodo: '',
+		todoArrayList : [
+			'Fare la spespa',
+			'Fare i compiti',
+			'Fare il bucato'
+		],
+	},
+	methods : {
+	},
+	created() {
+	}
+})
